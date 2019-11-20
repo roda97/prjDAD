a8e1161d56e8f8e4ec7b6caf8130e90d9687a7b6
Marcelo Lima <marcelo.lima1412@gmail.com>
2019-11-20 15:38:27 +0000

    Revert "Erro de 2 includes"
    This reverts commit 5503fa08abafd5b116442f4dd2fd60b4533e165a.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
