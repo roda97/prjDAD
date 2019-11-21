05de2be0b08f2b08d240880939eec7170a1b04ee
Roda <2150702@my.ipleiria.pt>
2019-11-21 17:51:23 +0000

    movimentoController e vuecontroller

---
+++ app/Movimento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movimento extends Model
+{
+    protected $fillable = [
+        'id', 'wallet_id', 'type','transfer', 'transfer_movement_id', 'transfer_wallet_id',
+        'type_payment','category_id','iban','mb_entity_code','mb_payment_reference',
+        'description', 'source_description','date','start_balance','end_balance','value',
+    ];
+}
---
+++ resources/views/vue/index.blade.php
@@ -0,0 +1,18 @@
+@extends('master')
+
+@section('title', 'Vue.js App')
+
+@section('content')
+
+<!--<users> </users> isto desaparece e passa a ser as linhas a baixo (ainda interligado com os conts router/routes-->
+
+<router-link to="/">Users</router-link>
+<router-link to="/movimentos">Movimentos</router-link>
+<!-- estas duas linhas de cima, criam-me as hiperligações que me aparecem no projeto -->
+
+<router-view></router-view> <!-- como eu não queria que aparece os users em todas as paginas, uso este comando para passar a usar as rotas -->
+
+@endsection
+@section('pagescript')
+<script src="js/vue.js"></script>
+ @stop  
