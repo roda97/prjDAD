a7d0fc2b504d8d25d5d581e8ff6b9c9e00738886
Roda <2150702@my.ipleiria.pt>
2020-01-06 20:42:07 +0000

    .

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Laravel\Passport\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-//use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
